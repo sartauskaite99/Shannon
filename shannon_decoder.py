f0f64e48d30bf5bb8214a226bb90904cf0265b35
Ausr <ausrine333@gmail.com>
2021-01-19 18:30:52 +0200

    shannon code

--- shannon_decoder.py
+++ shannon_decoder.py
@@ -5,9 +5,7 @@ import numpy
 import zlib
 import pickle
 
-
 def main():
-    mydict = None
     fileToWrite = sys.argv[2]
     fileToRead = sys.argv[1]
 
@@ -15,60 +13,81 @@ def main():
     fileRead = open(fileToRead, "rb")
 
     data = fileRead.read()
+
+    mydict, head, ahead = getDictionary(data)
+
+    text = getText(head, data)
+
+    letter = getDecodedText(text,mydict)
+
+    file = writeToFile(file,letter)
+    fileRead.close()
+    file.flush()
+    file.close();
+
+def getDictionary(file):
+    mydict = None
+    dictInterval = 0
     head = 2
     ahead = 0
-    dictInterval = 0
-
     while mydict is None:
-        search = data[ahead:head]
+        search = file[ahead:head]
         search = search.decode('latin1')
 
         if search == '\n\n':
-            mydict = data[dictInterval:head]
+            mydict = file[dictInterval:head]
             mydict = dict2File(mydict)
-            #mydict = json.loads(mydict)
             print(mydict)
             break
+
         head = head + 1
         ahead = ahead + 1
+    return mydict, head, ahead
+
+def getText(head,file):
+    binaryText = file[head:(len(file))]
+    text =''
+
+    for char in binaryText:
+        text += bin(ord(chr(char)))[2:].zfill(7)
+    print(text)
+
+    return text
+
+def getDecodedText(text,mydict):
+    letter = ""
     searchb = 0
     searchIterator = 0
-    datab = data[head:(len(data))]
-    st =''
-    array = []
-
-    for char in datab:
-        st += bin(ord(chr(char)))[2:].zfill(7)
-    print(st)
-    letter = ""
-    while searchb in range(len(st)):
-        searchBuffer = st[searchIterator:searchb]
+    while searchb in range(len(text)):
+        searchBuffer = text[searchIterator:searchb]
         stringBuffer = searchBuffer
 
         if stringBuffer in mydict:
             print(mydict[stringBuffer])
             letter += str(mydict[stringBuffer])
-
             searchIterator = searchb;
 
         searchb = searchb + 1
+
+    return letter
+
+def dict2File(file):
+    bytes = zlib.decompress(file)
+    return pickle.loads(bytes)
+
+def writeToFile(file,letter):
+    array = []
     a = 0
     while a in range(len(letter)):
-        while len(array)<8 and a<len(letter):
+        while len(array) < 8 and a < len(letter):
             array.append(int(letter[a]))
-            a=a+1
+            a = a+1
         print(array)
         char = bytearray(numpy.packbits(array)).decode('latin1')
         print(char)
         byte = bytes(char, 'latin1')
         file.write(byte)
         array.clear()
-    fileRead.close()
-    file.flush()
-    file.close();
-
-def dict2File(file):
-    bytes = zlib.decompress(file)
-    return pickle.loads(bytes)
+    return file
 
 main()
